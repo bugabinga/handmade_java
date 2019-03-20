1cd85a6494f247ba7cbc34b2821b9e5eb2d6ced2
Oliver Jan Krylow <oliver@bugabinga.net>
2019-03-20 22:46:35 +0100

    build: strict options and build directory
    The build system puts compiled class files now into the `bld` directory.
    Why `bld, you ask? For the same reason sources are kept in `src`. This
    is one of those old conventions worth discussing. Is there a benefit of
    `src` over `sources`?
    
    Furthermore, we add a bunch of strict options regarding warning, docs
    and linting, that `javac` checks for us.
    It is intentionally too strict now and will loosen up over time.
    
    The build is hard-coded to be a "debug" build for now.

--- build.java
+++ build.java
@@ -1,16 +1,88 @@
 import static java.lang.System.out;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.lang.System.err;
+import static java.nio.file.Paths.get;
+import static java.nio.file.Files.deleteIfExists;
+import static java.nio.file.Files.walkFileTree;
+import static java.nio.file.Files.exists;
+import static javax.tools.ToolProvider.getSystemJavaCompiler;
+import static java.text.MessageFormat.format;
 
-import javax.tools.ToolProvider;
+import java.nio.charset.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.util.*;
+import java.io.*;
+import javax.tools.*;
 
-class build {
-  private static String[] SOURCES = new String[] {"src/Main.java"};
-
-  public static void main(String[] args) {
+class build
+{
+  public static void main(String[] args)
+  throws IOException
+  {
     out.println("BUILDING THE HANDMADE JAVA PROJECT!");
-    var javac = ToolProvider.getSystemJavaCompiler();
-    var file_manager = javac.getStandardFileManager(null, null, UTF_8);
-    var compilation_units = file_manager.getJavaFileObjects(SOURCES);
-    javac.getTask(null, file_manager, null, null, null, compilation_units).call();
+ 		var src = new Path[]{get("src/Main.java")};
+ 		var bld = get("bld");
+ 		var locale = Locale.getDefault();
+    var utf8 = StandardCharsets.UTF_8;
+		/*
+		* These flags are the same as one would use with the `javac` cli interface.
+		* TODO: this is a debug build right now. we will need different flags for other builds (release,test,etc.)
+		* These settings are unrealistically strict right now.
+		* The idea is to start out with a very strict set of rules.
+		* And then, as we get increaingly annoyed by stupid rules, we deactivate those.
+		* This should converge to a resonable set of rules.
+		* TODO: different parts of the application probably will require different strictness. we can apply them per package/module/naming-pattern/...
+		*/
+		var options = List.of(
+			"-g", //generate debug info
+			"-Werror", //quit on warnings
+			"-Xdoclint:all", //warn about malformed docs
+			"--doclint-format", "html5", //use modern docs
+			"-Xlint:all", //lint our code
+			"-Xpkginfo:always", //require package-info.java files so that generated Javadoc gets nice comments for packages.
+			"-verbose", //to learn what happens
+			"--limit-modules", "java.base", //by limiting the modules, we prevent loading of unused ones.
+			"-deprecation", //print uses of deprecated code
+			"-d", bld.toString(), //output class files to build folder
+			"-encoding", utf8.toString(), //expected encoding of source files
+			"--release", "11" //complile for given version.
+												 //This way we can upgrade the JDK without immediatly breaking the project.
+			);
+    var javac = getSystemJavaCompiler();
+		var diagnostics = new DiagnosticCollector<JavaFileObject>();
+    var file_manager = javac.getStandardFileManager(diagnostics, locale, utf8);
+    var compilation_units = file_manager.getJavaFileObjects(src);
+    deleteDir(bld);
+    javac.getTask(new PrintWriter(err), file_manager, diagnostics, options, null, compilation_units).call();
+    diagnostics.getDiagnostics().stream()
+    .map(d -> format("{0} [{1}] ''{2}:{3}'' MSG: {4}",
+    								 d.getKind(),
+    								 d.getCode(),
+    								 d.getSource().getName(),
+    								 d.getLineNumber(),
+    								 d.getMessage(locale)))
+	  .forEach(err::println);
+  }
+
+  static void deleteDir(Path dir)
+  throws IOException
+  {
+  	walkFileTree(dir,
+      new SimpleFileVisitor<>() {
+        @Override public FileVisitResult postVisitDirectory( Path dir, IOException __)
+        throws IOException
+        {
+            deleteIfExists(dir);
+            return FileVisitResult.CONTINUE;
+        }
+         
+        @Override public FileVisitResult visitFile( Path file, BasicFileAttributes __)
+        throws IOException
+        {
+            deleteIfExists(file);
+            return FileVisitResult.CONTINUE;
+        }
+    });
+ 		assert !exists(dir): "The directory still exists!";
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+/**
+* Welcome to the entry point of <b>handmade_java</b>!
+*/
 class Main {
+	/** @param args ignored  */
   public static void main(String[] args){
     System.out.println("I AM APPLICATION");
   }
